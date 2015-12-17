bcdc32ddc70139491fbf4806cfb0b2bde3f8444e
Wen <417455771@qq.com>
2015-12-17 20:46:59 +0800

    Social Framwork Test
    - SLComposeViewController
    - SLServiceTypeSinaWeibo
    - UIAlertController
        - UIAlertAction
    - URLScheme jump to settings.
    - Configure layer properties: layer.backroundColor(CGColor)/cornerRadius(圆角)/borderColor/borderWidth

---
+++ SocialNetWorkTest/SocialNetWorkTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocialNetWorkTest
+//
+//  Created by Wen Tan on 12/17/15.
+//  Copyright © 2015 Wen Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
