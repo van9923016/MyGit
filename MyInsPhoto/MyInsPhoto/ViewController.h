bde12bfd567951a8f72882294d7993fa5f22cbc2
Wen <417455771@qq.com>
2015-12-19 16:30:00 +0800

    MyInsPhoto project
    Basic UI.

---
+++ MyInsPhoto/MyInsPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyInsPhoto
+//
+//  Created by Wen Tan on 12/19/15.
+//  Copyright © 2015 Wen Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyInsPhoto/MyInsPhoto/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyInsPhoto
+//
+//  Created by Wen Tan on 12/19/15.
+//  Copyright © 2015 Wen Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
