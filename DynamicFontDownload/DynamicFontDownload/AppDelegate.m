5da65d14cc77e1df608d1db479366a95fe445fe9
Wen <417455771@qq.com>
2015-12-30 23:41:56 +0800

    Dynamic font download Project
    Basic UI

--- DynamicFontDownload/DynamicFontDownload/AppDelegate.m
+++ DynamicFontDownload/DynamicFontDownload/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,7 +16,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-	// Override point for customization after application launch.
+	ViewController *mainVC = [[ViewController alloc] initWithNibName:@"ViewController"
+															  bundle:nil];
+	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+	self.window.rootViewController = mainVC;
+	[self.window makeKeyAndVisible];
 	return YES;
 }
 
--- DynamicFontDownload/DynamicFontDownload/ViewController.m
+++ DynamicFontDownload/DynamicFontDownload/ViewController.m
@@ -8,14 +8,45 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UITableView *fontTable;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *indicator;
+@property (weak, nonatomic) IBOutlet UIButton *downloadBtn;
+@property (weak, nonatomic) IBOutlet UIProgressView *progress;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
 
 @end
 
 @implementation ViewController
 
+
+
+#pragma mark - UITableView delegate
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+	UITableViewCell *cell = [self.fontTable dequeueReusableCellWithIdentifier:@"Cell"];
+	return cell;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	
+}
+
+#pragma mark - UITableView datasource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	//TODO:
+	return 0;
+}
+
+
+#pragma mark - App lifecycle
 - (void)viewDidLoad {
     [super viewDidLoad];
+	self.fontTable.delegate = self;
+	self.fontTable.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning {
