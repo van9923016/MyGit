1e8326bab557d1d2153e53a1d219a69cda189d03
Wen <417455771@qq.com>
2015-12-30 23:41:20 +0800

    DynamicFontDownload Project
    Basic UI

---
+++ DynamicFontDownload/DynamicFontDownload/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  mainViewController.h
+//  DynamicFontDownload
+//
+//  Created by Wen Tan on 12/30/15.
+//  Copyright © 2015 Wen Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DynamicFontDownload/DynamicFontDownload/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  mainViewController.m
+//  DynamicFontDownload
+//
+//  Created by Wen Tan on 12/30/15.
+//  Copyright © 2015 Wen Tan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
