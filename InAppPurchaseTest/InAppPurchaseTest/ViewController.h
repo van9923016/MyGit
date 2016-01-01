f1318913574caea5221c8fa8722ab9baaa85f2d4
Wen <417455771@qq.com>
2016-01-01 23:49:48 +0800

    In App purchase logic
    Basic logic for in app purchase

---
+++ InAppPurchaseTest/InAppPurchaseTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InAppPurchaseTest
+//
+//  Created by Wen Tan on 1/1/16.
+//  Copyright © 2016 Wen Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
