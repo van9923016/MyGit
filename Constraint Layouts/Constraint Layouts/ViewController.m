aba17d262aa61399ffe7c5272e8803bc0bc928cf
Wen <417455771@qq.com>
2016-01-16 14:31:53 +0800

    autolayout
    different views in portrait and landscape mode

---
+++ Constraint Layouts/Constraint Layouts/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Constraint Layouts
+//
+//  Created by Wen Tan on 1/16/16.
+//  Copyright © 2016 Wen Tan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
