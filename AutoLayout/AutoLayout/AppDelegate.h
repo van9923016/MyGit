66406533e8d46ec06eee87766f9c2f117017f5d0
Wen <417455771@qq.com>
2016-01-16 12:38:56 +0800

    SizeClass

---
+++ AutoLayout/AutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by Wen Tan on 1/16/16.
+//  Copyright © 2016 Wen Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout/AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Wen Tan on 1/16/16.
+//  Copyright © 2016 Wen Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
