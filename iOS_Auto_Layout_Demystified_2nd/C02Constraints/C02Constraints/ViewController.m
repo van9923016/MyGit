a4327e19e341353ab201659ade860ce9e523417f
Wen <417455771@qq.com>
2016-02-10 22:19:28 +0800

    add Constraint programmtically

--- iOS_Auto_Layout_Demystified_2nd/C02Constraints/C02Constraints/ViewController.m
+++ iOS_Auto_Layout_Demystified_2nd/C02Constraints/C02Constraints/ViewController.m
@@ -10,17 +10,46 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIButton *button;
+
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	//setContentCompressionResistancePriority
-	//setContentHuggingPriority
-	
+	self.view.backgroundColor = [UIColor redColor];
+	self.button = [[UIButton alloc] init];
+	[self.button setTitle:@"Log in" forState:UIControlStateNormal];
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+	self.button.translatesAutoresizingMaskIntoConstraints = NO;
+	self.button.layer.cornerRadius = 8.0;
+	self.button.backgroundColor = [UIColor blackColor];
+	//center Y
+	NSLayoutConstraint *logBtnYConstraint = [NSLayoutConstraint constraintWithItem:self.button attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY  multiplier:1.0 constant:0];
+	//center X
+	NSLayoutConstraint *logBtnXConstraint = [NSLayoutConstraint constraintWithItem:self.button attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX  multiplier:1.0 constant:0];
+	//button width 70
+	NSLayoutConstraint *logBtnWidth = [NSLayoutConstraint constraintWithItem:self.button attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:70.f];
+	//button height 70
+	NSLayoutConstraint *logBtnHeight = [NSLayoutConstraint constraintWithItem:self.button attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:70.f];
+	[self.view addSubview:self.button];
+	[self.view addConstraint:logBtnXConstraint];
+	[self.view addConstraint:logBtnYConstraint];
+	[self.view addConstraint:logBtnWidth];
+	[self.view addConstraint:logBtnHeight];
+	NSLog(@"%@",[self.view.subviews description]);
+}
+
+- (void)viewWillLayoutSubviews {
+
+}
+- (void)viewDidLayoutSubviews {
+	
+}
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
 	// Dispose of any resources that can be recreated.
