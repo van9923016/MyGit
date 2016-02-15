14b44af7d16437cc8480c1f906f8a18bc20c24e5
Wen <417455771@qq.com>
2016-02-15 08:56:55 +0800

    done

--- iToDo/iToDo/TWListEditingView.m
+++ iToDo/iToDo/TWListEditingView.m
@@ -22,71 +22,81 @@
 @end
 @implementation TWListEditingView
 
-- (void)checkButtonState {
+- (void)adjustUI {
+	self.notesField.layer.borderWidth = 2.0f;
+	self.notesField.layer.cornerRadius = 8.0f;
+	self.notesField.layer.borderColor = [UIColor grayColor].CGColor;
+}
+
+- (void)changeButtonState {
 	UIImage *buttonImage = [self.checked boolValue] ? [UIImage imageNamed:@"Checked Checkbox 2"] : nil ;
 	[self.checkButton setImage:buttonImage forState:UIControlStateNormal];
 }
-- (void)createNewList {
-	List *list = [[AppDelegate sharedInstance] createList];
-	list.title = self.titleField.text;
-	list.priority = [NSNumber numberWithInteger:self.prioritySeg.selectedSegmentIndex];
-	list.notes = self.notesField.text;
-	list.alarmDate = self.datePicker.date;
-	list.isChecked = self.checked;
-	list.edittingTime = [NSDate date];
+
+- (void)showSelectedList {
+	self.titleField.text = self.editingList.title;
+	self.prioritySeg.selectedSegmentIndex = [self.editingList.priority integerValue];
+	self.datePicker.date = self.editingList.alarmDate;
+	self.notesField.text = self.editingList.notes;
+	self.checked = self.editingList.isChecked;
+	[self changeButtonState];
+}
+
+- (void)saveList {
+	self.editingList.title = self.titleField.text;
+	self.editingList.priority = [NSNumber numberWithInteger:self.prioritySeg.selectedSegmentIndex];
+	self.editingList.notes = self.notesField.text;
+	self.editingList.alarmDate = self.datePicker.date;
+	self.editingList.isChecked = self.checked;
+	self.editingList.edittingTime = [NSDate date];
 	[[AppDelegate sharedInstance] saveContext];
 }
 
-- (void)deleteObject {
-	NSError *error = nil;
-	NSArray *dataArray = [[[AppDelegate sharedInstance] managedObjectContext] executeFetchRequest:[NSFetchRequest fetchRequestWithEntityName:@"List"] error:nil];
-	for (NSManagedObject *a in dataArray) {
-		if ([a isEqual:self.editingList]) {
-			[[[AppDelegate sharedInstance] managedObjectContext] deleteObject:a];
-			[[[AppDelegate sharedInstance] managedObjectContext] save:&error];
-		}
-	}
-	self.editingList = nil;
-}
-
-- (IBAction)deleteList:(UIButton *)sender {
-	//delete stored data
-	[self deleteObject];
-	[self.navigationController popViewControllerAnimated:YES];
-}
-- (IBAction)saveList:(UIBarButtonItem *)sender {
-	
-	if (self.editingList) {
-		[self deleteObject];
-	}
-	[self createNewList];
-
-	[self.navigationController popViewControllerAnimated:YES];
+- (void)deleteList {
+	[[[AppDelegate sharedInstance] managedObjectContext] deleteObject:self.editingList];
+	[[AppDelegate sharedInstance] saveContext];
 }
 
 - (IBAction)checkButtonPressed:(UIButton *)sender {
 	self.checked = [NSNumber numberWithBool:![self.checked boolValue]];
-	[self checkButtonState];
+	[self changeButtonState];
 }
+
+- (IBAction)deleteList:(UIButton *)sender {
+	//delete stored data
+	[self deleteList];
+	self.editingList = nil;
+	[self.navigationController popViewControllerAnimated:YES];
+}
+- (IBAction)saveList:(UIBarButtonItem *)sender {
+	[self saveList];
+	[self.navigationController popViewControllerAnimated:YES];
+}
+
+
 #pragma mark - View Life Cycles
 - (void)viewDidLoad {
 }
 
 - (void)viewWillAppear:(BOOL)animated {
 	[super viewWillAppear:YES];
-	self.notesField.layer.borderWidth = 2.0f;
-	self.notesField.layer.cornerRadius = 8.0f;
-	self.notesField.layer.borderColor = [UIColor grayColor].CGColor;
+	[self adjustUI];
 	
 	if (self.editingList) {
-		self.titleField.text = self.editingList.title;
-		self.prioritySeg.selectedSegmentIndex = [self.editingList.priority integerValue];
-		self.datePicker.date = self.editingList.alarmDate;
-		self.notesField.text = self.editingList.notes;
-		self.checked = self.editingList.isChecked;
-		[self checkButtonState]; 
+		[self showSelectedList];
+	} else {
+		self.editingList = [[AppDelegate sharedInstance] createList];
 	}
+	
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+	[super viewWillDisappear:animated];
+	if (self.editingList)
+		[self saveList];
+	
 
 }
+
 
 @end
